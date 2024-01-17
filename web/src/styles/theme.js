966074f653fab7662ad09890eeca3fd0bc8d58f3
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-17 13:50:04 -0300

    initial commit

---
+++ web/src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+    COLORS: {
+        RED_DARK: "#DD524C",
+        RED_LIGHT: "#F7D4D3",
+
+        BLUE_DARK: "#3662E3",
+        BLUE_LIGHT: "#E3E8EF",
+
+        BLACK: "#00000033",
+
+        YELLOW_DARK: "#E9A23B",
+        YELLOW_LIGHT: "#F5D565",
+
+        GREEN_DARK: "#32D657",
+        GREEN_LIGHT: "#A0ECB1",
+
+        GRAY: "#97A3B6",
+        BEIGE: "#F5E8D5",
+
+        WHITE: "#F8FAFC"
+    }
+}
