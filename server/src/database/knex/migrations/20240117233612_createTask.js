059998bda026eb430cf6a6d777bf6d8a60b97f8c
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-17 20:41:28 -0300

    feat: create task migration

---
+++ server/src/database/knex/migrations/20240117233612_createTask.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = knex => knex.schema.createTable("tasks", table => {
+    table.increments("id");
+    table.text("description");
+    table.text("title");
+    table.text("icon_name");
+    table.integer("board_id").references("id").inTable("boards");
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable("tasks");
