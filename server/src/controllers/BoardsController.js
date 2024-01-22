c7e88c5413600579f5d68f9ab4dd54698532d158
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-22 18:44:51 -0300

    feat: board update route

--- server/src/controllers/BoardsController.js
+++ server/src/controllers/BoardsController.js
@@ -2,7 +2,10 @@ const knex = require("../database/knex");
 
 class BoardController {
     async create (request, response) {
-        const [board_id] = await knex("boards").insert({});
+        const [board_id] = await knex("boards").insert({
+            title: "My Task Board",
+            description: "Tasks to keep organised"
+        });
 
         const defaultTasks = [
             {
@@ -35,6 +38,31 @@ class BoardController {
             message: "board created successfully",
             board_id
         })
+    }
+
+    async show (request, response) {
+        const { board_id } = request.params;
+        const board = await knex("boards").where({ id: board_id }).first();
+
+        return response.json({
+            board
+        })
+    }
+
+    async update (request, response) {
+        const { board_id } = request.params;
+        const { title, description } = request.body;
+
+        await knex("boards").update({
+            title,
+            description
+        }).where( { id: board_id });
+
+        return response.json({
+            status: "sucess",
+            message: "Board atualizado com sucesso"
+        })
+
     }
 }
 
--- server/src/routes/board.routes.js
+++ server/src/routes/board.routes.js
@@ -6,5 +6,7 @@ const BoardController = require("../controllers/BoardsController");
 const boardController = new BoardController();
 
 boardRoutes.post("/", boardController.create);
+boardRoutes.put("/:board_id", boardController.update);
+boardRoutes.get("/:board_id", boardController.show);
 
 module.exports = boardRoutes;
